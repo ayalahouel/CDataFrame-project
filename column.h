668a8c01d6a13d89bbf7dc9ed241ddf6ba9aa06a
ayalahouel <151038731+ayalahouel@users.noreply.github.com>
2024-04-21 19:40:05 +0200

    Update column.h

--- column.h
+++ column.h
@@ -1,5 +1,9 @@
 #ifndef COLUMN_H
 #define COLUMN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 typedef struct {
     char title[50];
@@ -18,5 +22,35 @@ int count_greater_than(Column *col, int x);
 int count_less_than(Column *col, int x);
 int count_equal_to(Column *col, int x);
 
+
+
+typedef enum {
+    NULLVAL = 1, UINT, INT, CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
+} ENUM_TYPE;
+
+typedef union {
+    unsigned int uint_value;
+    signed int int_value;
+    char char_value;
+    float float_value;
+    double double_value;
+    char* string_value;
+    void* struct_value;
+} COL_TYPE;
+
+typedef struct {
+    char *title;
+    unsigned int size;
+    unsigned int max_size;
+    ENUM_TYPE column_type;
+    COL_TYPE **data;
+    unsigned long long int *index;
+} COLUMN;
+
+COLUMN *create_column(ENUM_TYPE type, char *title);
+int insert_value(COLUMN *col, void *value);
+void delete_column(COLUMN **col);
+
 #endif /* COLUMN_H */
+
 
